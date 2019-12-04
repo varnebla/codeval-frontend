c02c8c3cc8f287c4c503575c3f455ee07d9406c0
Victor Arnedo Blanco <victor@Air-de-Victor.lan>
2019-12-04 13:26:48 +0100

    feat: style some buttons and start landing styling

--- src/presentational/ExcercisesList/ExcercisesList.js
+++ src/presentational/ExcercisesList/ExcercisesList.js
@@ -2,21 +2,15 @@ import React from 'react';
 
 import ExercisesListItem from '../ExcercisesListItem/ExcercisesListItem';
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './ExercisesList.css';
 
 function ExercisesList ( { exercises }) {
   return (
-    // <div className="exerciseList-container">
-    <Row>
+    <div className="exerciseList-container">
       {exercises && exercises.map(oneExer => (
-        <Col xs={12} sm={6} lg={4} key={oneExer._id}>
-          <ExercisesListItem  exercise={oneExer}/>
-        </Col>
+        <ExercisesListItem key={oneExer._id} exercise={oneExer}/>
       ))}
-    </Row>
-    //</div>
+    </div>
   );
 }
 
