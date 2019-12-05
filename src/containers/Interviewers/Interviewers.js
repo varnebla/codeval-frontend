4b5c70583fd7d2928e6f993757999e6e0b0c8783
Gmiliuta <53524766+Gmiliuta@users.noreply.github.com>
2019-12-05 12:09:36 +0100

    hotfixes and styling

--- src/containers/Interviewers/Interviewers.js
+++ src/containers/Interviewers/Interviewers.js
@@ -14,6 +14,15 @@ function Interviewers () {
   const [interviewers, setInterviewers] = useState([]);
   const [createModal, setCreateModal] = useState(false);
 
+  // STATE FOR MARKER LINES AND USE EFFECT TO CALL IT ONLY ONCE
+  const [random, setRandom]= useState('');
+
+  useEffect(() => {
+    if (!random) {
+      setRandom(randomMarker());
+    }
+  }, []);
+
   useEffect(()=>{
     getInterviewers()
       .then(results => setInterviewers(results));
@@ -27,6 +36,10 @@ function Interviewers () {
         :
         <Container className="top-bar-padding ">
           <div className="interviewers-top-bar">
+            <div>
+              <h1 className="highlighted-text">Interviewers</h1>
+              <div className={'highlight-report' + ' '+ random } style={{marginTop: '-42px', background: '#ab8bf7', width: '250px', marginLeft: '-15px'}}></div>
+            </div>
             <Button variant="primary" onClick={()=>{setCreateModal(true);}}>Create Interviewer</Button>
           </div>
           {interviewers && <InterviewersList interviewers={interviewers}/>}
@@ -39,3 +52,9 @@ function Interviewers () {
 }
 
 export default Interviewers;
+
+// HELPER FUNCTION TO GET RANDOM POSTION OF THE MARKER AND USE EFFECT TO MAKE IT
+function randomMarker () {
+  const arr = ['h-r', 'h-r1', 'h-r2', 'h-r3', 'h-r4', 'h-r5', 'h-r6', 'h-r7', 'h-r8' ,'h-r9'];
+  return arr[Math.floor(Math.random() * 10)];
+}
